d1ee3e915475421a362621e840b832f66c11f7d1
mdakash102210-wq <mdaksh102210@gmail.com>
2026-02-06 17:00:12 -0800

    fast comit

---
+++ assigntment03.js
@@ -0,0 +1,20 @@
+function newPrice(currentPrice, discount) {
+  let notNumber = "Invalid";
+  if (typeof currentPrice !== "number") {
+    return notNumber;
+  }
+  if (typeof discount !== "number") {
+    return notNumber;
+  }
+  if (discount < 0 || discount > 100) {
+    return notNumber;
+  }
+
+  let discountPrice = (currentPrice * discount) / 100;
+  let newPrice = currentPrice - discountPrice;
+  newPrice = newPrice.toFixed(3);
+  return newPrice;
+}
+let result = newPrice(500, "5");
+
+console.log(result);
