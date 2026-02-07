984dcaf9d767e3697c95bb0baa8d5e16872fc32a
mdakash102210-wq <mdaksh102210@gmail.com>
2026-02-06 18:32:23 -0800

    commit five

--- assigntment03.js
+++ assigntment03.js
@@ -22,22 +22,50 @@
 
 // 2 no problem start
 
-function validOtp(otp) {
-  let stateschek = "Invalid";
-  let otpValid = true;
-  let startwidth = "ph-";
+// function validOtp(otp) {
+//   let stateschek = "Invalid";
+//   let otpValid = true;
+//   let startwidth = "ph-";
 
-  if (typeof otp != "string") {
-    return stateschek;
-  } else if (otp.length < 8) {
-    otpValid = false;
-  } else if (!otp.startsWith(startwidth)) {
-    otpValid = false;
+//   if (typeof otp != "string") {
+//     return stateschek;
+//   } else if (otp.length < 8) {
+//     otpValid = false;
+//   } else if (!otp.startsWith(startwidth)) {
+//     otpValid = false;
+//   }
+//   return otpValid;
+// }
+
+// let result = validOtp(12345678);
+// console.log(result);
+
+// // 3 no problem start
+
+function finalScore(omr) {
+  let count = 0;
+  let valids = "Invalid";
+  if (Array.isArray(omr) == true) {
+    return valids;
   }
-  return otpValid;
+  if (typeof omr !== "object") {
+    return valids;
+  }
+  for (let score in omr) {
+    count = omr[score] + count;
+  }
+  if (count > 100 || count < 100) {
+    return valids;
+  }
+  let wrongResult = omr["wrong"] * 0.5;
+  let rightResult = omr["right"];
+  let skipResukt = omr["skip"] * 0;
+  let Totalresult = rightResult - wrongResult - skipResukt;
+  Totalresult = Math.round(Totalresult);
+  let fainalResult = Totalresult;
+  return fainalResult;
 }
-
-let result = validOtp(12345678);
+let result = finalScore(["Raj"]);
 console.log(result);
 
-// 3 no problem start
+// // 4 no problem start
