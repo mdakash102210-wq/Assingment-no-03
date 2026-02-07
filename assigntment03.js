d1c4a5c9c379507d762311df07b0f5cb5d4ec3bf
mdakash102210-wq <mdaksh102210@gmail.com>
2026-02-06 17:42:43 -0800

    commit four

--- assigntment03.js
+++ assigntment03.js
@@ -1,25 +1,43 @@
-function newPrice(currentPrice, discount) {
-  let notNumber = "Invalid";
-  if (typeof currentPrice !== "number") {
-    return notNumber;
-  }
-  if (typeof discount !== "number") {
-    return notNumber;
-  }
-  if (discount < 0 || discount > 100) {
-    return notNumber;
-  }
+// function newPrice(currentPrice, discount) {
+//   let notNumber = "Invalid";
+//   if (typeof currentPrice !== "number") {
+//     return notNumber;
+//   }
+//   if (typeof discount !== "number") {
+//     return notNumber;
+//   }
+//   if (discount < 0 || discount > 100) {
+//     return notNumber;
+//   }
 
-  let discountPrice = (currentPrice * discount) / 100;
-  let newPrice = currentPrice - discountPrice;
-  newPrice = newPrice.toFixed(3);
-  let fainalNewPrice = newPrice;
-  return fainalNewPrice;
-}
-let result = newPrice(1500, 20);
+//   let discountPrice = (currentPrice * discount) / 100;
+//   let newPrice = currentPrice - discountPrice;
+//   newPrice = newPrice.toFixed(3);
+//   let fainalNewPrice = newPrice;
+//   return fainalNewPrice;
+// }
+// let result = newPrice(1500, 20);
 
-console.log(result);
-
-// git push dane
+// console.log(result);
 
 // 2 no problem start
+
+function validOtp(otp) {
+  let stateschek = "Invalid";
+  let otpValid = true;
+  let startwidth = "ph-";
+
+  if (typeof otp != "string") {
+    return stateschek;
+  } else if (otp.length < 8) {
+    otpValid = false;
+  } else if (!otp.startsWith(startwidth)) {
+    otpValid = false;
+  }
+  return otpValid;
+}
+
+let result = validOtp(12345678);
+console.log(result);
+
+// 3 no problem start
